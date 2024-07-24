8ec9dc328791052f4a6c119f86c7f6dd52028f67
Gitntro-Yukti <gitintro2024@gmail.com>
2024-07-24 16:43:32 +0530

    to test git repo from local

---
+++ src/test/java/in/yuktisoftwares/GitIntro/GitIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package in.yuktisoftwares.GitIntro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
